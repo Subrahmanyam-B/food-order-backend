180dcaaff1847891e027e2c84f67b8be7d7cc2a4
Subrahmanyam Bitra <subrahmanyam9124@gmail.com>
2024-06-16 14:16:46 +0530

    Deployment to Coolfiy

--- dist/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = exports.APP_SECRET = exports.MONGO_URI = void 0;
-exports.MONGO_URI = "mongodb://localhost:27017/online_food_delivery";
-exports.APP_SECRET = "our_app_secret";
-exports.PORT = process.env.PORT || 8000;
-//# sourceMappingURL=index.js.map
